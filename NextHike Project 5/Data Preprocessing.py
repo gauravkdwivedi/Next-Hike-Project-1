4e07372a08fc53864e82d579f0d344621b74830f
Gaurav Kumar <gauravdwivedi91@hotmail.com>
2024-01-27 16:57:03 +0530

    git commit

--- NextHike Project 5/Data Preprocessing.py
+++
@@ -1,9 +0,0 @@
-# import necessary liabraries
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# Load the dataset
-df = pd.read_excel('C:/Users/gaura/OneDrive/Desktop/telcom_data.xlsx')
-df
